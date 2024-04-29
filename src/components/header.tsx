fdb01fdab76e914d2444ac09a77b01ed75bcd649
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-04-29 13:46:35 -0300

    style: add responsiveness to Header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,25 +2,25 @@ import logo from "../assets/images/logo.svg";
 
 export function Header() {
   return (
-    <header className="flex flex-col gap-4 w-full">
-      <div className="flex justify-center items-center p-20 bg-black mb-44">
-        <img src={logo} alt="logo" className="w-96" />
-      </div>
+    <div className="flex flex-col gap-4 w-full">
+      <header className="flex justify-center items-center h-32 bg-black mb-16">
+        <img src={logo} alt="logo" className="w-56" />
+      </header>
 
-      <div className="flex flex-col justify-start px-12 gap-14">
-        <h1 className="text-6xl font-gotham-bold text-dark-grey">
+      <div className="flex flex-col justify-start px-8 gap-6">
+        <h1 className="text-4xl font-gotham-bold text-dark-grey">
           REABERTURA
           <br /> SMART FIT
         </h1>
 
-        <span className="w-52 h-5 bg-dark-grey" />
+        <span className="w-28 h-4 bg-dark-grey" />
       </div>
 
-      <p className="my-20 px-12 text-3xl font-gotham-book">
+      <p className="my-12 px-8 text-xl font-gotham-book">
         O horário de funcionamento das nossas unidades está seguindo os decretos
         de cada município. Por isso, confira aqui se a sua unidade está aberta e
         as medidas de segurança que estamos seguindo.
       </p>
-    </header>
+    </div>
   );
 }
