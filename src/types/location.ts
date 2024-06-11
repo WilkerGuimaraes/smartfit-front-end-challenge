06f85983eab37548145a6def37dc1cd9cad3048d
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-06-11 14:23:19 -0300

    refactor: update Locations type

--- src/types/location.ts
+++ src/types/location.ts
@@ -1,16 +1,28 @@
-interface ILocationSchedule {
-  weekdays: string;
-  hour: string;
+export interface LocationRoot {
+  current_country_id: number;
+  locations: ILocation[];
+  wp_total: number;
+  total: number;
+  success: boolean;
 }
-
 export interface ILocation {
   id: string;
   title: string;
-  content: string;
-  opened: boolean;
-  mask: "required" | "recommended";
-  towel: "required" | "recommended";
-  fountain: "not_allowed" | "partial";
-  locker_room: "closed" | "allowed" | "partial";
-  schedules: ILocationSchedule[];
+  content?: string;
+  opened?: boolean;
+  mask?: "required" | "recommended";
+  towel?: "required" | "recommended";
+  fountain?: "not_allowed" | "partial";
+  locker_room?: "closed" | "allowed" | "partial";
+  schedules?: ILocationSchedule[];
+  street?: string;
+  region?: string;
+  city_name?: string;
+  state_name?: string;
+  uf?: string;
+}
+
+interface ILocationSchedule {
+  weekdays: string;
+  hour: string;
 }
