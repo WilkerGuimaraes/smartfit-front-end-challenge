f46a87f345916a5b07de237eaa5ef032c653739d
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-06-14 20:10:57 -0300

    style: update header component style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,7 @@
 export function Header() {
   return (
-    <div className="flex flex-col gap-4 w-full">
-      <header className="flex justify-center items-center h-32 bg-black mb-16">
-        <img src="/logo.svg" alt="logo" className="w-56" />
-      </header>
-    </div>
+    <header className="flex justify-center items-center h-32 bg-black mb-16">
+      <img src="/logo.svg" alt="logo" className="w-56" />
+    </header>
   );
 }
